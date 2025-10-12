c2cdfa74eb6349e4e724fcc0c37f30a7d580571c
opraupe0017 <78523312+opraupe0017@users.noreply.github.com>
2025-10-12 12:51:14 -0500

    fix: Ajuste del daño de Santiago en el método set_posicion en la clase Peon

--- peon.py
+++ peon.py
@@ -5,7 +5,7 @@ class Peon():
         self.esta_activo = True
 
     def set_posicion(self, posicion_nueva):
-        letra = posicion_nueva[0]
+        letra = ord(posicion_nueva[0])
         numero = posicion_nueva[1]
 
         letra_actual = ord(self.posicion[0])
