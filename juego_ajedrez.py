dc06b8331a93e754c1b58a65004e3cedba2706ca
opraupe0017 <78523312+opraupe0017@users.noreply.github.com>
2025-10-11 22:08:26 -0500

    feat: Plantilla clase BasicChessGame

---
+++ juego_ajedrez.py
@@ -0,0 +1,18 @@
+class BasicChessGame():
+    def __init__(self):
+        pass # Implementar código desde esta línea
+    
+    def set_fichas(self):
+        """Cargar las fichas en el tablero.
+        """
+        pass # Implementar código desde esta línea
+
+    def jugar(self):
+        """Activador del juego de ajedrez básico.
+        """
+        pass # Implementar código desde esta línea
+
+
+if __name__ == '__main__':
+    juego = BasicChessGame()
+    print(juego.jugar())
