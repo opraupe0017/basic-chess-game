9d6a6a3068b1fed7a3b7d72bc42813c7a4eb905d
AlunaDxd <santiagolopera64@gmail.com>
2025-10-19 11:10:38 -0500

    test: inserte la ficha rey

--- tablero.py
+++ tablero.py
@@ -1,7 +1,7 @@
 from peon import Peon
 from torre import Torre
 from caballo import Caballo
-
+from rey import Rey
 
 class Tablero():
     def __init__(self):
@@ -97,11 +97,13 @@ if __name__ == '__main__':
         Caballo(posicion=['b', 1], es_blanco=True),
         Caballo(posicion=['g', 1], es_blanco=True),
         Torre(posicion=['h', 1], es_blanco=True),
+        Rey(posicion=['e', 1], es_blanco=True),
 
         Peon(posicion=['a', 7], es_blanco=False),
         Caballo(posicion=['a', 4], es_blanco=False),
         Caballo(posicion=['g', 8], es_blanco=False),
         Torre(posicion=['h', 8], es_blanco=False),
+        Rey(posicion=['e', 8], es_blancoo=False),
     ]
     for ficha in fichas:
         tablero.set_ficha(ficha)
