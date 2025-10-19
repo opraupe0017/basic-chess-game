c02f3215e127fa5ad56755803b3add310005862d
opraupe0017 <78523312+opraupe0017@users.noreply.github.com>
2025-10-19 10:32:55 -0500

    feat: Implementar __str__ en clase Alfil.

--- alfil.py
+++ alfil.py
@@ -1,19 +1,22 @@
 
 class Alfil():
-    def __init__(self,posición=['c', 1] es_blanco=True):
-        self. es_blanco = es_blanco 
-        self. posición = posición
+    def __init__(self, posicion=['c', 1], es_blanco=True):
+        self.es_blanco = es_blanco 
+        self.posicion = posicion
         self. esta_activo = True 
 
-def set_posición(self, posición_nueva): 
-    letra = posición_nueva[0]
-    número = posición_nueva[1] 
+    def set_posicion(self, posicion_nueva): 
+        letra = posicion_nueva[0]
+        numero = posicion_nueva[1] 
 
-    letra_actual = ord(self.posición[0])
-    numero_actual = self. posición[1]
+        letra_actual = ord(self.posicion[0])
+        numero_actual = self. posicion[1]
+        
+        #Movimiento del Alfil 
+        if abs(letra - letra_actual) == abs(numero - numero_actual) and letra != letra_actual: 
+            self.posicion = posicion_nueva 
+        else:
+            print('ERROR.PosiciÃ³n invalida')
     
-    #Movimiento del Alfil 
-    if abs(letra - letra_actual) == abs(número - numero_actual) and letra != letra_actual: 
-        self.posición = posición_nueva 
-    else:
-        print('ERROR.Posición invalida')
+    def __str__(self):
+        return "â™—" if self.es_blanco else "â™"
