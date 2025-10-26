4b69ba76c2c48831e8495899078844a69ea76ae3
alejoguti17gaviria-ia <alejoguti17gaviria@gmail.com>
2025-10-26 09:21:42 -0500

    feat: implementacion del modulo jugar en class BasicChessGame

--- juego_ajedrez.py
+++ juego_ajedrez.py
@@ -10,7 +10,29 @@ class BasicChessGame():
     def jugar(self):
         """Activador del juego de ajedrez básico.
         """
-        pass # Implementar código desde esta línea
+        turno_blancos = True
+        while self.rey_blanco.esta_activo ==  True and self.rey_negro.esta_activo == True:
+            se_rinde = input("¿Deseas rendirte?")
+            if se_rinde == "si" and turno_blancos == True:
+                self.rey_blanco.esta_activo = False
+            elif se_rinde == "si" and turno_blancos == False:
+                self.rey_negro.esta_activo = False
+            else:
+                desde = input("Ingresa posicion desde: ")
+                hasta = input("Ingresa posicion hasta: ")
+                cond_1 = self.tablero.fichas.get(desde,None) is not None
+                cond_2 = self.tablero.fichas[desde].es_blanco == turno_blancos
+                if cond_1 and cond_2:
+                    try:
+                        self.tablero.set_mover(desde, hasta)
+                        turno_blancos = False
+                    except:
+                        print(f"Posición {hasta} fuera del tablero.")
+        
+        if self.rey_blanco.esta_activo == False:
+            print("Ganador Rey negro")
+        else:
+            print("Ganador Rey blanco")         
 
 
 if __name__ == '__main__':
