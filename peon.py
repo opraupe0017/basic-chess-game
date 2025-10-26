67e9ce5df6cf65db129d04cd3fbeabb49168b247
alejoguti17gaviria-ia <alejoguti17gaviria@gmail.com>
2025-10-26 11:33:45 -0500

    fix: agregue doble paso para el peon en clase Peon

--- peon.py
+++ peon.py
@@ -1,8 +1,9 @@
-class Peon():
-    def __init__(self, posicion=["a",2], es_blanco=True):
+class Peon:
+    def __init__(self, posicion=["a", 2], es_blanco=True):
         self.es_blanco = es_blanco
         self.posicion = posicion
         self.esta_activo = True
+        self.primer_movimiento = True 
 
     def set_posicion(self, posicion_nueva):
         letra = ord(posicion_nueva[0])
@@ -12,13 +13,21 @@ class Peon():
         numero_actual = self.posicion[1]
 
         num = 1 if self.es_blanco else -1
+
         if (numero_actual + num) == numero and letra_actual == letra:
             self.posicion = posicion_nueva
+            self.primer_movimiento = False
+
+        elif self.primer_movimiento and (numero_actual + 2 * num) == numero and letra_actual == letra:
+            self.posicion = posicion_nueva
+            self.primer_movimiento = False
+
         elif (numero_actual + num) == numero and abs(letra - letra_actual) == 1:
             self.posicion = posicion_nueva
+            self.primer_movimiento = False
+
         else:
-            raise Exception("ERROR, posicion invalida. ")
+            raise Exception("ERROR: movimiento inválido para el peón.")
 
     def __str__(self):
-        return "♙" if self.es_blanco else "♟"
-        
+        return "♙" if self.es_blanco else "♟︎"
