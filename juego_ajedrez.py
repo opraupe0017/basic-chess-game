c414899e7706d544d42f17310bb7ded225d86274
salinas444 <salinas.work444@gmail.com>
2025-10-26 09:29:18 -0500

    feat: implementación inserción de fichas negras en el tablero de ajedrez

--- juego_ajedrez.py
+++ juego_ajedrez.py
@@ -5,7 +5,36 @@ class BasicChessGame():
     def set_fichas(self):
         """Cargar las fichas en el tablero.
         """
-        pass # Implementar código desde esta línea
+        #Fichas de Peones Negros 
+        self.tablero.set_ficha(Peon(posicion=['a', 7], es_blanco=False))
+        self.tablero.set_ficha(Peon(posicion=['b', 7], es_blanco=False))
+        self.tablero.set_ficha(Peon(posicion=['c', 7], es_blanco=False))
+        self.tablero.set_ficha(Peon(posicion=['d', 7], es_blanco=False))
+        self.tablero.set_ficha(Peon(posicion=['e', 7], es_blanco=False))
+        self.tablero.set_ficha(Peon(posicion=['f', 7], es_blanco=False))
+        self.tablero.set_ficha(Peon(posicion=['g', 7], es_blanco=False))
+        self.tablero.set_ficha(Peon(posicion=['h', 7], es_blanco=False))
+
+        #Fichas de Caballos Negros 
+        self.tablero.set_ficha(caballo(posicion=['b', 8], es_blanco=False))
+        self.tablero.set_ficha(caballo(posicion=['g', 8], es_blanco=False))
+
+        #Fichas de las Torres Negras 
+        self.tablero.set_fichas(Torre(posicion=['h', 8], es_blanco=False))
+        self.tablero.set_fichas(Torre(posicion=['a', 8], es_blanco=False))
+
+        #Fichas de Alfiles Negros
+        self.tablero.set_ficha(Alfil(posicion=['c', 8], es_blanco=False))
+        self.tablero.set_ficha(Alfil(posicion=['f', 8], es_blanco=False))
+
+        #Ficha del Rey Negro
+        self.tablero.set_ficha(Rey(posicion=["e", 8], es_blanco=False))
+
+        #Ficha de la Reina Negra 
+        self.tablero.set_ficha(reina(posicion=['d', 8], es_blanco=False))
+
+        self.rey_negro = self.tablero["e8"]
+    
 
     def jugar(self):
         """Activador del juego de ajedrez básico.
